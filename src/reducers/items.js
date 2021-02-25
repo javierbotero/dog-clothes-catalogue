4eca11ae0845dc0b411da7d5cdce57d44e7b8328
Javier Botero <javierbotero10271@gmail.com>
2021-02-25 11:46:50 -0500

    Fix initial items state in items.js reducer

--- src/reducers/items.js
+++ src/reducers/items.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
-import sweater1 from '../assets/images/sweater1.jpg';
 import { retrieveItems } from '../actions/actions';
 
 const initialState = {
@@ -11,14 +10,14 @@ const initialState = {
       category: '',
       price: 100,
       description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
-      image: sweater1,
+      image: ['Coats and Jackets', 1],
     },
     {
       name: '',
       category: '',
       price: 100,
       description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
-      image: sweater1,
+      image: ['Coats and Jackets', 1],
     },
   ],
   error: '',
